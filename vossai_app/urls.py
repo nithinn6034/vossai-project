2ee699215d27a2c7aadbfe2a2611ea387bc6241c
nithin <nithinn6034@gmail.com>
2026-05-18 22:49:59 +0530

    fix: use plain function view for catch-all to fix recursion error

--- vossai_app/urls.py
+++ vossai_app/urls.py
@@ -1,15 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.views.generic import TemplateView
+from django.shortcuts import render
+
+def frontend(request):
+    return render(request, 'index.html')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('vossai_app.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
+    re_path(r'^.*$', frontend),
 ]
-
-# Explicitly define error handlers to avoid inspection recursion
-handler400 = 'django.views.defaults.bad_request'
-handler403 = 'django.views.defaults.permission_denied'
-handler404 = 'django.views.defaults.page_not_found'
-handler500 = 'django.views.defaults.server_error'
