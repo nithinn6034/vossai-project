41e8db233c776ca592f0814c8e7d6fc128da6fd0
nithin <nithinn6034@gmail.com>
2026-05-18 22:41:25 +0530

    fix urls recursion error

--- vossai_app/urls.py
+++ vossai_app/urls.py
@@ -1,12 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.views.generic import TemplateView
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('vossai_app.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
-    path('<path:path>', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
