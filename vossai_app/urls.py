46aa6964ca6852153ec3cab74b61993c1f05e149
nithin <nithinn6034@gmail.com>
2026-05-18 22:47:05 +0530

    fix: replace TemplateView catch-all with plain function view

--- vossai_app/urls.py
+++ vossai_app/urls.py
@@ -5,5 +5,11 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('vossai_app.urls')),
-    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='frontend'),
 ]
+
+# Explicitly define error handlers to avoid inspection recursion
+handler400 = 'django.views.defaults.bad_request'
+handler403 = 'django.views.defaults.permission_denied'
+handler404 = 'django.views.defaults.page_not_found'
+handler500 = 'django.views.defaults.server_error'
